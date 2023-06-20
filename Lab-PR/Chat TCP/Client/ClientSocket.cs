c603ef8de0c039d344ac8bd7b05b74af39b7c2c9
Nik1v <137095339+Nik1v@users.noreply.github.com>
2023-06-20 15:48:50 +0300

    Add files via upload

---
+++ Lab-PR/Chat TCP/Client/ClientSocket.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    public class ClientSocket
+    {
+        private readonly Socket _clientSocket;
+
+        public ClientSocket()
+        {
+            _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Connect(string ip, int port)
+        {
+            var ipAddress = IPAddress.Parse(ip);
+            var endPoint = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                _clientSocket.Connect(endPoint);
+                Console.WriteLine($"Conectat la server {endPoint}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Eroare de conectare {e.Message}");
+            }
+        }
+
+        public void Send()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Scrie mesajul: ");
+
+                    string message = Console.ReadLine() ?? "";
+
+                    byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+                    _clientSocket.Send(buffer);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Eroare mesaj {e.Message}");
+                }
+            }
+        }
+
+        public void Receive()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = new byte[1024];
+                    int byteReceived = _clientSocket.Receive(buffer);
+                    string message = Encoding.UTF8.GetString(buffer, 0, byteReceived);
+
+                    if (!string.IsNullOrEmpty(message))
+                    {
+                        Console.Write($"\nMesaj: {message} \nScrie mesajul: ");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nServerul a inchis conexiunea");
+                    return;
+                }
+            }
+        }
+    }
+}
