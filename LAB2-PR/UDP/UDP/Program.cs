d18fb87e8252db085342f92c75a2f8bde0f143df
Nik1v <137095339+Nik1v@users.noreply.github.com>
2023-06-20 15:52:42 +0300

    Add files via upload

---
+++ LAB2-PR/UDP/UDP/Program.cs
@@ -0,0 +1,28 @@
+﻿using UDP;
+
+string multicastIP = "239.3.5.6";
+int port = 5000;
+
+var chat = new UDPChat(multicastIP, port);
+
+chat.StartReceiveLoop();
+
+Console.WriteLine("Format =>  <IP>:<TEXT>"); // 192.168.1.201:SALUT, scriu în privat
+Console.WriteLine("0: General"); // 0:salutare, scriu în canalul general
+
+while(true)
+{
+    var input = Console.ReadLine() ?? "";
+    var splitted = input.Split(':');
+    var ip = splitted[0];
+    var text = splitted[1];
+
+    if (ip == "0")
+    {
+        chat.SendGeneral(text);
+    }
+    else
+    {
+        chat.SendTo(ip, text);
+    }
+}
---
+++ LAB2-PR/UDP/UDP/UDPChat.cs
@@ -0,0 +1,100 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP
+{
+    internal class UDPChat
+    {
+        private string _multicastIP;
+        private int _port;
+        private Socket _multicastSocket;
+        private Socket _senderSocket;
+
+        public UDPChat(string multicastIP, int port)
+        {
+               _multicastIP = multicastIP;
+               _port = port;
+
+               try
+               {
+                    _multicastSocket = new Socket(AddressFamily.InterNetwork,
+                         SocketType.Dgram, ProtocolType.Udp);
+
+                    var hostIP = new IPEndPoint(IPAddress.Parse("192.168.100.78"), port); // adresa IP de la rețeaua locală
+                    _multicastSocket.Bind(hostIP);
+
+                    _multicastSocket.SetSocketOption(SocketOptionLevel.IP,
+                         SocketOptionName.AddMembership,
+                         new MulticastOption(IPAddress.Parse(_multicastIP), IPAddress.Parse("192.168.100.78"))); // adresa IP de la rețeaua locală
+
+                    _senderSocket = new Socket(AddressFamily.InterNetwork,
+                         SocketType.Dgram, ProtocolType.Udp);
+
+                    _senderSocket.SetSocketOption(SocketOptionLevel.IP,
+                         SocketOptionName.AddMembership,
+                         new MulticastOption(IPAddress.Parse(_multicastIP), IPAddress.Any));
+               }
+               catch (Exception e)
+               {
+                    Console.WriteLine($"Error occurred while setting up the sockets: {e.Message}");
+               }
+        }
+
+        public void StartReceiveLoop()
+        {
+            Task.Run(() => receive());
+        }
+
+        private void receive()
+        {
+               try
+               {
+                    while (true)
+                    {
+                         byte[] buffer = new byte[1024];
+                         EndPoint remoteEndpoint = new IPEndPoint(0, 0);
+
+                         _multicastSocket.ReceiveFrom(buffer, ref remoteEndpoint);
+                         string text = Encoding.UTF8.GetString(buffer);
+
+                         Console.WriteLine($"{remoteEndpoint}: {text}");
+                    }
+               }
+               catch(Exception e)
+               {
+                    Console.WriteLine($"Error occurred while receiving data: {e.Message}");
+               }
+        }
+
+        public void SendTo(string ip, string text)
+        {
+               try
+               {
+                    byte[] bytes = Encoding.UTF8.GetBytes(text);
+                    var remoteEndpoint = new IPEndPoint(IPAddress.Parse(ip), _port);
+
+                    _senderSocket.SendTo(bytes, remoteEndpoint);
+               }
+               catch(Exception e) 
+               {
+                    Console.WriteLine($"Error occurred while sending data: {e.Message}");
+               } 
+        }
+
+        public void SendGeneral(string text)
+        {
+               try
+               {
+                    byte[] bytes = Encoding.UTF8.GetBytes(text);
+                    var remoteEndpoint = new IPEndPoint(IPAddress.Parse(_multicastIP), _port);
+
+                    _senderSocket.SendTo(bytes, remoteEndpoint);
+               }
+               catch (Exception e)
+               {
+                    Console.WriteLine($"Error occurred while sending data: {e.Message}");
+               }
+          }
+    }
+}
