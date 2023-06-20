7c2ab613236b88ee3058f220623562a53977d11b
Nik1v <137095339+Nik1v@users.noreply.github.com>
2023-06-20 15:49:00 +0300

    Add files via upload

---
+++ Lab-PR/Chat TCP/Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Client;
+
+ClientSocket client = new ClientSocket();
+
+Thread clientT = new Thread(() =>
+{
+    client.Connect("127.0.0.1", 5050);
+
+    Thread send = new Thread(client.Send);
+    Thread receive = new Thread(client.Receive);
+
+    send.Start();
+    receive.Start();
+});
+clientT.Start();
